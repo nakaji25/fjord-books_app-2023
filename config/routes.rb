8297da9b7a301ed327dd5527c995caefc6f01a4b
nakaj1st <0619nakaji@gmail.com>
2025-05-21 13:12:56 +0900

    再設定用メールによるパスワード再設定機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    passwords: 'users/passwords'
+  }
+  resources :users
   resources :books
   root 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 end
