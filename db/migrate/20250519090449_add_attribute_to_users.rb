b8b819d4d0753b7fe35d4663fc8234bbdb6511a3
nakaj1st <0619nakaji@gmail.com>
2025-05-27 13:46:12 +0900

    maigrateファイルを統合

--- db/migrate/20250519090449_add_attribute_to_users.rb
+++ db/migrate/20250519090449_add_attribute_to_users.rb
@@ -2,5 +2,7 @@ class AddNameToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :name, :string
     add_column :users, :profile, :text
+    add_column :users, :zip_code, :string
+    add_column :users, :address, :text
   end
 end
--- db/migrate/20250519093942_add_address_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddAddressToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :zip_code, :string
-    add_column :users, :address, :text
-  end
-end
